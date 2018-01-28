dba1b9f7a4cb5be1b4bff159c27c997eb948049b
Khoa Bui <khoa.bui.dev@gmail.com>
2018-01-28 11:16:54 +0700

    Implement HMVC modular

---
+++ application/modules/block/controllers/Block.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Block extends MX_Controller {
+
+	public function hello()
+	{
+		return '<h1>Hello this is Codeigniter 2.2.6 final with HVMC </h1>';
+	}
+}
